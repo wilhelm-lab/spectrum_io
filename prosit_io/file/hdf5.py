71f633586e028e4d91a64e69258a1bf530f37aba
Mario Picciani <mario.picciani@tum.de>
2021-07-14 11:41:37 +0200

    finalized read and write function for hdf5

--- prosit_io/file/hdf5.py
+++ prosit_io/file/hdf5.py
@@ -1,55 +1,92 @@
 import pandas as pd
+import numpy as np
 import h5py
 from scipy.sparse import coo_matrix
+from typing import Union, Optional, List
+import scipy
 import pandas as pd
+import logging
+import threading
 
-def read_file(path, start_index=0, end_index=-1):
+logger = logging.getLogger(__name__)
+
+def read_file(path: str, key: str) -> pd.DataFrame:
     """
-    Read hdf5 file and return df with contents.
+    Read hdf5 file and return dataframe with contents.
     With possibility to partial load for memory issues.
-    :param path:
-    :param start_index:
-    :param end_index:
-    :return:
+    :param path: The path to the hdf5 file to read.
+    :param key: The key of the dataset/group of interest.
+    :return: a pandas DataFrame
     """
-    with h5py.File(path, 'r') as f:
-        df = None
-        for index in range(start_index, end_index):
-            ## TODO indexing
-            ## TODO type checks and numpy convert
-            ## categorical data ?
-            if f[index].startswith("sparse"):
-                values = f[index]["values"]
-                i = f[index]["i"]
-                j = f[index]["j"]
-                shape = f[index]["shape"]
-                sparse_data = coo_matrix((values, (i, j)), shape=shape)
-                current_df = pd.DataFrame.sparse.from_spmatrix(sparse_data)
-            else:
-                current_df = pd.DataFrame(f[index])
-            df = current_df if df is None else pd.concat(df, current_df, axis=1)
+    try:
+        if key.startswith("sparse"):
+            with h5py.File(path, 'r') as f:
+                values = f[f"{key}/values"]
+                i = f[f"{key}/i"]
+                j = f[f"{key}/j"]
+                shape = f[f"{key}/shape"]
+                sparse_data = coo_matrix((values, (i, j)), shape)
+                df = pd.DataFrame.sparse.from_spmatrix(sparse_data)
+                if f"{key}/names" in f.keys():
+                    df.columns = f[f"{key}/names"]
+                if f"{key}/index" in f.keys():
+                    df.index = f[f"{key}/index"]
+        else:
+            df = pd.read_hdf(path, key=key)
+        return df
+    except Exception as e:
+        logger.exception(e)
 
-    return df
+def thread_this(fn):
+    def run(*args, **kwargs):
+        t = threading.Thread(target=fn, args=args, kwargs=kwargs)
+        t.start()
+        return t
+    return run
 
-
+@thread_this
 def write_file(
     data:Union[pd.DataFrame, scipy.sparse.coo_matrix],
     path: str,
     dataset_name: str,
     mode: str = 'w',
+    compression: Optional[Union[str, None]] = None,
+    names: Optional[Union[List[str], None]] = None,
+    index: Optional[Union[List[str], None]] = None
 ):
-    ## TODO categorical data?
-    with h5py.File(path, mode) as f:
+    """
+    Writes or appends data to an hdf5 file.
+    :params
+        data: The data to store. Can be a pandas DataFrame or a scipy Sparasematrix.
+        path: The path to store the file to.
+        dataset_name: The key in the hdf5 file under which to store the data.
+        mode: The method when writing the data. Use 'a' to append to an existing file or 'w' to overwrite.
+        compression: Optional, the method for compressing data. Check pandas.DataFrame.to_hdf docs for supported
+            compression methods in case of providing a pandas DataFrame and h5py Dataset docs in case of providing
+            a sparse matrix. Default: None.
+        names: Optional, addtional column names. Ignored if providing a pandas DataFrame. Default: None.
+        index: Optional, additional index. Ignored if providing a pandas DataFrame. Default: None.
+    """
+    try:
         if isinstance(data, pd.DataFrame):
-            f.create_dataset(dataset_name, data=np.asarray(data, compression="gzip", chunks=True, maxshape=(None,))
+            data.to_hdf(path, key=dataset_name, mode=mode, complib=compression)
         elif isinstance(data, scipy.sparse.coo_matrix):
-            group_name = f"sparse_{dataset_name}"
-            f.create_group(group_name)
-            i,j, val = scipy.sparse.find(data)
-            shape = data.shape
-            f.create_dataset(f"{group_name}/i", i)
-            f.create_dataset(f"{group_name}/j", j)
-            f.create_dataset(f"{group_name}/values", values)
-            f.create_dataset(f"{group_name}/shape", shape)
+            with h5py.File(path, mode) as f:
+                group_name = f"sparse_{dataset_name}"
+                f.create_group(group_name)
+                i,j, values = scipy.sparse.find(data)
+                shape = data.shape
+                print(shape)
+                f.create_dataset(f"{group_name}/i", data=i, dtype=int)
+                f.create_dataset(f"{group_name}/j", data=j, dtype=int)
+                f.create_dataset(f"{group_name}/values", data=values, dtype=float)
+                f.create_dataset(f"{group_name}/shape", data=shape, shape=(2,))
+                if names:
+                    f.create_dataset(f"{group_name}/names", names)
+                if index:
+                    f.create_dataset(f"{group_name}/index", index)
         else:
             assert False, "Only pd.DataFrame and scipy.sparse.coo_matrix are supported."
+        logger.info(f"Data {'appended' if mode=='a' else 'written'} to {path}")
+    except Exception as e:
+        logger.exception(e)
