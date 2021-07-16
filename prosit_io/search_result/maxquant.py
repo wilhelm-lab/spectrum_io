3a255d6ccc0e10e7b7b810ecb7e54165720f921e
WassimG <wassim.gabriel@gmail.com>
2021-07-16 17:38:34 +0200

    Fixed io to work with ce_calib

--- prosit_io/search_result/maxquant.py
+++ prosit_io/search_result/maxquant.py
@@ -3,6 +3,8 @@ import numpy as np
 from typing import List
 from .search_results import SearchResults
 from fundamentals import ALPHABET_MODS, MAXQUANT_VAR_MODS
+from fundamentals.mod_string import internal_without_mods
+
 
 
 class MaxQuant(SearchResults):
@@ -15,32 +17,39 @@ class MaxQuant(SearchResults):
         :return: DataFrame
         """
         df = pd.read_csv(path,
-                         usecols=lambda x: x.upper() in['RAW FILE',
-                                  'SCAN NUMBER',
-                                  'MODIFIED SEQUENCE',
-                                  'CHARGE',
-                                  'FRAGMENTATION',
-                                  'MASS ANALYZER',
-                                  'MASS', # TODO get column with experimental Precursor mass instead
-                                  'SCORE',
-                                  'REVERSE'],
-                         sep = "\t")
+                         usecols=lambda x: x.upper() in ['RAW FILE',
+                                                         'SCAN NUMBER',
+                                                         'MODIFIED SEQUENCE',
+                                                         'CHARGE',
+                                                         'FRAGMENTATION',
+                                                         'MASS ANALYZER',
+                                                         'MASS',
+                                                         # TODO get column with experimental Precursor mass instead
+                                                         'SCORE',
+                                                         'REVERSE',
+                                                         'RETENTION TIME'],
+                         sep="\t")
 
         # Standardize column names
         df.columns = df.columns.str.upper()
         df.columns = df.columns.str.replace(" ", "_")
 
-
-        df["REVERSE"].fillna(False,inplace=True)
+        df['MASS_ANALYZER'] = 'FTMS'
+        df['FRAGMENTATION'] = 'HCD'
+        df['RETENTION_TIME'] = [x for x in range(len(df))]
+        df["REVERSE"].fillna(False, inplace=True)
         df["REVERSE"].replace("+", True, inplace=True)
-        df.rename(columns={"CHARGE":"PRECURSOR_CHARGE"}, inplace=True)
+        df.rename(columns={"CHARGE": "PRECURSOR_CHARGE"}, inplace=True)
         df["MODIFIED_SEQUENCE"] = MaxQuant.transform_maxquant_mod_string(df["MODIFIED_SEQUENCE"])
+        df['SEQUENCE'] = internal_without_mods(df['MODIFIED_SEQUENCE'].values)
+        # TODO: calculate mass for modified peptide
+        df['CALCULATED_MASS'] = df['MASS']
         return df
 
     @staticmethod
     def transform_maxquant_mod_string(
-        sequences: np.ndarray,
-        fixed_mods: List[str] = [],
+            sequences: np.ndarray,
+            fixed_mods: List[str] = [],
     ):
         """
         Function to translate a MaxQuant modstring to the Prosit format
@@ -48,7 +57,8 @@ class MaxQuant(SearchResults):
 
         :return: pd.Series
         """
-        assert all(x in ALPHABET_MODS.keys() for x in fixed_mods), f"Provided illegal fixed mod, supported modifications are {str(ALPHABET_MODS.keys())}."
+        assert all(x in ALPHABET_MODS.keys() for x in
+                   fixed_mods), f"Provided illegal fixed mod, supported modifications are {str(ALPHABET_MODS.keys())}."
         modified_sequences = []
         for seq in sequences:
             seq = seq.replace("_", "")
