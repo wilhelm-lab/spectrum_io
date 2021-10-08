b60944505a5747aed0bea7b89f331852397044a3
WassimG <wassim.gabriel@gmail.com>
2021-10-08 12:52:03 +0200

    Fixed RT reading

--- prosit_io/search_result/maxquant.py
+++ prosit_io/search_result/maxquant.py
@@ -2,9 +2,15 @@ import pandas as pd
 import numpy as np
 from .search_results import SearchResults
 from fundamentals.mod_string import maxquant_to_internal, internal_without_mods
-
+import fundamentals.constants as C
 
 class MaxQuant(SearchResults):
+
+    @staticmethod
+    def add_tmt_mod(mass, seq):
+        num_of_tmt = seq.count('UNIMOD:737')
+        mass += (num_of_tmt * C.MOD_MASSES['[UNIMOD:737]'])
+        return mass
 
     @staticmethod
     def read_result(path: str):
@@ -37,12 +43,14 @@ class MaxQuant(SearchResults):
         if "FRAGMENTATION" not in df.columns:
             df['FRAGMENTATION'] = 'HCD'
 
-        df['RETENTION_TIME'] = [x for x in range(len(df))]
+        #df['RETENTION_TIME'] = [x for x in range(len(df))]
         df["REVERSE"].fillna(False, inplace=True)
         df["REVERSE"].replace("+", True, inplace=True)
         df["MODIFIED_SEQUENCE"] = maxquant_to_internal(df["MODIFIED_SEQUENCE"].to_numpy())
+        df["MASS"] = df.apply(lambda x: MaxQuant.add_tmt_mod(x.MASS, x.MODIFIED_SEQUENCE), axis=1)
         df["SEQUENCE"] = internal_without_mods(df["MODIFIED_SEQUENCE"])
         #Filter sequences remove sequences with length bigger than 30
         df['PEPTIDE_LENGTH'] = df["SEQUENCE"].apply(lambda x: len(x))
         df = df[df['PEPTIDE_LENGTH']<=30]
         return df
+
