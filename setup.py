646f3d1dcc2dad22a6ec841d64dea85481ef0260
WassimG <wassim.gabriel@gmail.com>
2022-01-20 14:19:17 +0100

    added pyteomics to dependencies

--- setup.py
+++ setup.py
@@ -19,7 +19,8 @@ install_requires = \
  'h5py>=3.3.0,<4.0.0',
  'numpy>=1.21.0,<2.0.0',
  'pandas>=1.3.0,<2.0.0',
- 'pymzml>=2.5.0,<3.0.0']
+ 'pymzml>=2.5.0,<3.0.0',
+ 'pyteomics>=4.3.3']
 
 setup_kwargs = {
     'name': 'prosit-io',
