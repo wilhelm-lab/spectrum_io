a4b1defb0f8dda13a12c2fa53cc26f69183fed1e
Mario Picciani <mario.picciani@tum.de>
2024-01-12 20:06:30 +0100

    Bump version from 0.5.0 to 0.4.1

--- spectrum_io/__main__.py
+++ spectrum_io/__main__.py
@@ -5,7 +5,7 @@ from rich import traceback
 
 
 @click.command()
-@click.version_option(version="0.5.0", message=click.style("spectrum_io Version: 0.5.0"))
+@click.version_option(version="0.4.1", message=click.style("spectrum_io Version: 0.4.1"))
 def main() -> None:
     """spectrum_io."""
 
