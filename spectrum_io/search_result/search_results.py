64104433acffa56ddc014f4afe6a1e15cab7ccdc
NormanRog <89592194+NormanRog@users.noreply.github.com>
2024-03-27 15:36:17 +0100

    Update search_results.py
    # for reading MSFragger pepxml files removed r"\[[0-9]+\]" from unsupported mods

--- spectrum_io/search_result/search_results.py
+++ spectrum_io/search_result/search_results.py
@@ -22,7 +22,8 @@ def filter_valid_prosit_sequences(df: pd.DataFrame) -> pd.DataFrame:
     # retain only peptides that fall within [7, 30] length supported by Prosit
     df = df[(df["PEPTIDE_LENGTH"] <= 30) & (df["PEPTIDE_LENGTH"] >= 7)]
     # remove unsupported mods to exclude
-    unsupported_mods = [r"Acetyl \(Protein N\-term\)", "ac", r"\[[0-9]+\]"]
+    # for reading MSFragger pepxml files removed r"\[[0-9]+\]" from unsupported mods
+    unsupported_mods = [r"Acetyl \(Protein N\-term\)", "ac"]
     exclude_mods_pattern = re.compile("|".join(unsupported_mods))
     df = df[~df["MODIFIED_SEQUENCE"].str.contains(exclude_mods_pattern)]
     # remove non-canonical aas
