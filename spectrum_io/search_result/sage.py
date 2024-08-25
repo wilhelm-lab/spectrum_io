4251a085f793f0ec24f4fccef1ddfdd8109a4d9e
Fabian Basso <fabian.basso@tum.de>
2024-08-25 19:23:06 +0000

    fixed mod_regex

--- spectrum_io/search_result/sage.py
+++ spectrum_io/search_result/sage.py
@@ -33,7 +33,7 @@ class Sage(SearchResults):
             are mapped automatically. To avoid this, explicitely provide an empty dictionary.
         :return: pd.DataFrame with the formatted data
         """
-        parsed_mods = parse_mods(self.standard_mods | (custom_mods or {}))
+        parsed_mods = parse_mods((custom_mods or {}) | self.standard_mods | (custom_mods or {}))
         if tmt_label:
             unimod_tag = c.TMT_MODS[tmt_label]
             parsed_mods[r"K\[\+\d+\.\d+\]"] = f"K{unimod_tag}"
